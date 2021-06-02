d661847c1df19d15885d40715b3ae91221c0ca9d
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-02 10:13:16 +0530

    Created class for sphere material

--- lib/screens/dashBoard/components/Body.dart
+++ lib/screens/dashBoard/components/Body.dart
@@ -1,19 +1,48 @@
-
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class Body extends StatefulWidget {
+  Body({Key? key}) : super(key: key);
+
   @override
   _BodyState createState() => _BodyState();
 }
-
 class _BodyState extends State<Body> {
+  late ArCoreController arCoreController;
+
+  _onArCoreViewCreated(ArCoreController _arcoreController) {
+    arCoreController = _arcoreController;
+    _addSphere(arCoreController);
+  }
+
+  _addSphere(ArCoreController _arcoreController) {
+    final material = ArCoreMaterial(color: Colors.blue,);
+    final sphere = ArCoreSphere(materials: [material], radius: 0.2,);
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(
+        0,
+        0,
+        -1,
+      ),
+    );
+
+    _arcoreController.addArCoreNode(node);
+  }
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Augmented Reality Flutter", style: TextStyle(color: Colors.black45,fontSize: 16,fontWeight: FontWeight.w600),),
-      ],
+    return Scaffold(
+      body: ArCoreView(
+        onArCoreViewCreated: _onArCoreViewCreated,
+      ),
     );
   }
 }
