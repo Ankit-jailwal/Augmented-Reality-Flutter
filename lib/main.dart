e2ca0d3a7d268ceb163a7da8da9ccf33a35f1505
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-02 06:02:35 +0530

    Added AR.png in Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Employee Emergency App',
+      title: 'Augmented Reality Flutter',
       home:SplashScreen(
           useLoader: true,
           loadingTextPadding: EdgeInsets.all(5),
           loadingText: Text("Loading",style: TextStyle(color: Colors.blueGrey,fontSize: 16,fontWeight: FontWeight.w400,fontFamily: "oswald"),),
           seconds: 3,
           navigateAfterSeconds: new dashBoard(),
-          title: Text("Employee Emergency App",style: TextStyle(color: Colors.black26,fontSize: 24,fontWeight: FontWeight.w600,fontFamily: "oswald"),),
-          image: new Image.asset("assets/images/Emergency.png"),
+          title: Text("Augmented Reality Flutter",style: TextStyle(color: Colors.black26,fontSize: 22,fontWeight: FontWeight.w600,fontFamily: "oswald"),),
+          image: new Image.asset("assets/images/AR.png"),
           photoSize: 100,
           backgroundColor: Colors.white,
           styleTextUnderTheLoader: new TextStyle(),
