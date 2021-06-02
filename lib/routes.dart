66959966dc0525796491b7940f87e77f98ae681c
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-02 05:35:21 +0530

    Created route for dashBoard

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:arflutter/screens/dashBoard/dashBoard.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  dashBoard.routeName: (context) => dashBoard(),
+};
+//Changes
