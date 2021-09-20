4060045ea91a3cd1198076e90297eb5e39c9f583
goyalkaran <goyalkaran2001@gmail.com>
2021-09-20 13:23:02 +0530

    minor fixes.

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Augmented Reality Flutter',
+      title: 'ARtistic',
       home:SplashScreen(
           useLoader: true,
           loadingTextPadding: EdgeInsets.all(5),
           loadingText: Text("Loading",style: TextStyle(color: Colors.blueGrey,fontSize: 16,fontWeight: FontWeight.w400,fontFamily: "oswald"),),
           seconds: 3,
           navigateAfterSeconds: new dashBoard(),
-          title: Text("Augmented Reality Flutter",style: TextStyle(color: Colors.black26,fontSize: 22,fontWeight: FontWeight.w600,fontFamily: "oswald"),),
+          title: Text("ARtistic",style: TextStyle(color: Colors.black26,fontSize: 22,fontWeight: FontWeight.w600,fontFamily: "oswald"),),
           image: new Image.asset("assets/images/AR.png"),
           photoSize: 150,
           backgroundColor: Color(0xfffffaf0),
